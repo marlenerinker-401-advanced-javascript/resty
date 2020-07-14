738494e42c10b90e272a8cffe7239e20a5d5912e
marlene-rinker <runner@fv-az60.k2jyrraalcpu1duxghdgrpli2h.cx.internal.cloudapp.net>
2020-07-14 22:49:17 +0000

    Updates

---
+++ precache-manifest.917e49aa22debf7128f7402f3e7121e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dcbbf97bd8b13a79a1d397cfaee42f7",
+    "url": "/resty/index.html"
+  },
+  {
+    "revision": "9dbc1d0e2eac88ca7439",
+    "url": "/resty/static/css/main.166b0ea6.chunk.css"
+  },
+  {
+    "revision": "669d34ed5e47a976c371",
+    "url": "/resty/static/js/2.0e955af2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/resty/static/js/2.0e955af2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dbc1d0e2eac88ca7439",
+    "url": "/resty/static/js/main.a7358f84.chunk.js"
+  },
+  {
+    "revision": "3238c6891d0a10119924",
+    "url": "/resty/static/js/runtime-main.d8529955.js"
+  }
+]);
